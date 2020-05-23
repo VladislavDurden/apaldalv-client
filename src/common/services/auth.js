1d1065056d960fd0c5cfd09039e10cfcf823bc3e
Vladislav Petrovskiy <vpetrovskiy@tula.co>
2020-05-24 01:21:34 +0300

    add requests service for authentication

---
+++ src/common/services/auth.js
@@ -0,0 +1,13 @@
+const apiUrl = 'https://apaldalv-api.herokuapp.com/auth';
+
+export const signUp = (email, password) => {
+	const endpointPath = '/register';
+
+	console.log(email, password);
+};
+
+export const signIn = (email, password) => {
+	const endpointPath = '/login';
+
+	console.log(email, password);
+};
