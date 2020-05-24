4eb71d32db93ee6118b1b99c0a16768369712eee
Vladislav Petrovskiy <vpetrovskiy@tula.co>
2020-05-25 01:27:09 +0300

    add account

--- src/common/services/auth.js
+++ src/common/services/auth.js
@@ -1,17 +1,21 @@
-const axios = require('axios');
+import history from '../../history';
+import axios from 'axios';
 const apiUrl = 'https://apaldalv-api.herokuapp.com/auth';
 
 export const signUp = (email, password) => {
 	const url = `${apiUrl}/register`;
+	const userData = {
+		email: email,
+		password: password,
+	};
 
-	axios({
-		method: 'post',
-		url: url,
-		data: {
-			email: email,
-			password: password,
-		}
-	});
+	return axios.post(url, userData)
+		.then(() => {
+			history.push('/account');
+		})
+		.catch((err) => {
+			console.log(err);
+		})
 };
 
 export const signIn = (email, password) => {
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 import App from './App';
 import LoginPage from './common/components/loginPage/loginPage.component';
+import Account from './common/components/account/account.component';
 
 export default [{
 	component: App,
@@ -7,5 +8,9 @@ export default [{
 		path: "/",
 		exact: true,
 		component: LoginPage,
+	}, {
+		path: "/account",
+		exact: true,
+		component: Account,
 	}]
 }]
